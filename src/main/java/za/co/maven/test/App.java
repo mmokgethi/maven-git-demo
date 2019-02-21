f742705ba525c2b5e47c077a8c53b7b9b53fa759
Modisaotsile Mokgethi <modisamokgethi@gmail.com>
2019-02-21 09:20:32 +0200

    initial commit

---
+++ src/main/java/za/co/maven/test/App.java
@@ -0,0 +1,10 @@
+package za.co.maven.test;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Test App");
+
+        System.out.println("Sum  " + 1+ 1);
+    }
+}
